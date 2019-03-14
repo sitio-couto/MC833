27f4bf7fb16b73b15c45be5d41ba3f7f2c6f6fc0
Sitio <v188115@g.unicamp.br>
2019-03-14 11:12:10 -0300

    Added server.h with includes and structs fixes

--- lab01/server/server.c
+++ lab01/server/server.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#define PORT 8080
+#include "server.h"
 
 int main(int argc, char *argv){
   int socketfd, new_socket, status;
@@ -12,28 +7,28 @@ int main(int argc, char *argv){
 
   int l, next;
 
-  menset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-  if (!(status = getaddrinfo(NULL, "3490", &hints, &servinfo))) {
-    printf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-    exit(1);
-  } else {
-    l = servinfo;
-    while (l) {
-      printf("Canonical name: %s\n", l.ai_canonname);
-
-    }
-  }
-
-  socketfd = socket(PF_INET, SOCK_STREAM, 0);
-
-
-  if (!socketfd) {
-    printf("erro\n");
-  } else {
-    printf("Sockerfd = %u\n", &socketfd);
-  }
+  // menset(&hints, 0, sizeof(hints));
+  // hints.ai_family = AF_UNSPEC;
+  // hints.ai_socktype = SOCK_STREAM;
+  // hints.ai_flags = AI_PASSIVE;
+  // if (!(status = getaddrinfo(NULL, "3490", &hints, &servinfo))) {
+  //   printf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+  //   exit(1);
+  // } else {
+  //   l = servinfo;
+  //   while (l) {
+  //     printf("Canonical name: %s\n", l.ai_canonname);
+  //
+  //   }
+  // }
+  //
+  // socketfd = socket(PF_INET, SOCK_STREAM, 0);
+  //
+  //
+  // if (!socketfd) {
+  //   printf("erro\n");
+  // } else {
+  //   printf("Sockerfd = %u\n", &socketfd);
+  // }
 
 }
