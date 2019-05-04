7cd1f60e86d854fb1abf83771b7eb282b2b6d7a8
vinicius espindola <vinicius.c.e@hotmail.com>
2019-05-04 11:13:58 -0300

    Fixed typo in tcp exit message

--- lab02/server/server_tcp.c
+++ lab02/server/server_tcp.c
@@ -91,7 +91,7 @@ void request_options(int socket) {
         printf("Message sent.\n");
         break;
       case 'e':
-        printf("Client (%d): ended connction\n",getpid());
+        printf("Client (%d): ended connection\n",getpid());
         return;
       default:
         gettimeofday(&t2, NULL);
