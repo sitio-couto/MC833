ce150cac25b0de8568e32137e87341cb78afa933
Sitio <v188115@g.unicamp.br>
2019-06-13 11:06:28 -0300

    Added client_script

---
+++ lab03/client_script.sh
@@ -0,0 +1,6 @@
+java client/Client localhost timers/c1_out.txt < timers/case1_in.txt
+java client/Client localhost timers/c2_out.txt < timers/case2_in.txt
+java client/Client localhost timers/c3_out.txt < timers/case3_in.txt
+java client/Client localhost timers/c4_out.txt < timers/case4_in.txt
+java client/Client localhost timers/c5_out.txt < timers/case5_in.txt
+java client/Client localhost timers/c6_out.txt < timers/case6_in.txt
